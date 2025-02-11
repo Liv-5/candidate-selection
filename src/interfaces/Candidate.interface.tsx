ff164fde29fd1d376afa50971d09f359d7a767e0
Liv-5 <osmith5@kent.edu>
2025-02-10 22:17:54 -0500

    workin on functions

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,13 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+  img?: string;
+
+  username?: string;
+
+  email?: string;
+
+  company?: string;
+
+  bio?: string;
+}
+export default Candidate;
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,8 +1,32 @@
-import { useState, useEffect } from 'react';
-import { searchGithub, searchGithubUser } from '../api/API';
+import { useState, useEffect } from "react";
+import { searchGithub, searchGithubUser } from "../api/API";
+import Candidate from "../interfaces/Candidate.interface";
 
 const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const [users, setUsers] = useState<Candidate[]>([]);
+  const [candidate, setCandidate] = useState<Candidate>();
+
+  useEffect(() => {
+    searchGithub().then((data) => {
+      setUsers(data);
+      console.log(data);
+
+      const results = searchGithubUser(users[0]);
+
+      setCandidate();
+    });
+  }, []);
+
+  useEffect(() => {
+    if (users.length > 0) {
+      searchGithubUser(users[0].username).then((data) => {
+        setCandidate(data);
+        console.log("Fetched Candidate:", data);
+      });
+    }
+  }, [users]);
+
+  return <h1>users[0]</h1>;
 };
 
 export default CandidateSearch;
